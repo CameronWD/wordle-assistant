2fbbde09c5c7a72e618ae40c915fd405c3d48ce7
Cam <cwilliams.developer@gmail.com>
2024-05-22 20:34:03 +1000

    updated copy

--- src/components/SuggestionsDisplay.jsx
+++ src/components/SuggestionsDisplay.jsx
@@ -4,7 +4,7 @@ import '../styles/SuggestionsDisplay.scss';
 function SuggestionsDisplay({ possibleWords }) {
     return (
         <div>
-            <h3 className="suggestions-title">Possible words...{possibleWords.length}</h3>
+            <h3 className="suggestions-title">Possible words remaining...{possibleWords.length}</h3>
             <div className="suggestions-container">
                 {possibleWords.map((word, index) => (
                     <div key={index} className="word-card">
